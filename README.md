0b24b69486a6f9d0c1623e4bad1b4cf4d9701745
Kyle Higginson <kyle@routereports.com>
2023-07-01 20:41:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# aws-lambda-pupeteer-serverless
+
+## Prereqs
+
+- You have [Serverless framework](https://www.serverless.com/framework/docs/getting-started) setup.
+
+
+## What is this repo?
+
+- This repo contains one AWS Lambda which runs Pupeteer to log in to [CauliCloud](https://caulicloud.com) and ensure an element appears once logged in.
+- The Lambda will run every 10 minutes using an EventBridge event
+- The Lambda runs on the NodeJS 18.x runtime.
+
+
+## Deploy
+
+From the root of the repo run:
+
+```
+npm install
+serverless deploy
+```
---
+++ run_pupeteer.js
@@ -0,0 +1,51 @@
+const chromium = require("chrome-aws-lambda")
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  })
+}
+
+exports.handler = async (event) => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  })
+
+  const page = await browser.newPage()
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+  })
+
+  await page.goto("https://caulicloud.com/authentication", {
+    waitUntil: "networkidle2",
+    timeout: 0,
+  })
+
+  await page.waitForSelector("#continueWithEmail")
+  await page.click("#continueWithEmail")
+
+  await page.type("#emailInput", "kylehigginson11@hotmail.com")
+  await page.click("#continueLoggingIn")
+
+  await page.waitForSelector("#passwordInput", { visible: true })
+  await page.type("#passwordInput", "wrong_password")
+
+  await page.waitForSelector("#continueLoggingIn", { visible: true })
+  await page.click("#continueLoggingIn")
+
+  await page.waitForSelector("#monitorsList", { visible: true, timeout: 20000 })
+
+  const element = await page.waitForSelector("#monitorsList > li:first-of-type")
+  await element.click()
+
+  await delay(5000)
+
+  await browser.close()
+
+  return event
+}
